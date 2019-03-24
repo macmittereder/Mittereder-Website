d2d17cc46468f54ff627ed89fec97f9493bdda1e
macmitt2300@gmail.com <macmitt2300@gmail.com>
2019-03-24 01:47:34 -0400

    Adding Visit information

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
-//var sassMiddleware = require('../middleware/sass');
-// compile sass in middleware for each page
+var geoip = require('geoip-lite');
+var MobileDetect = require('mobile-detect');
+var fs = require('fs');
 
 var router = express.Router();
 
 router.get('/', function (req, res) {
+  var md = new MobileDetect(req.headers['user-agent']);
+  var geo = geoip.lookup(req.ip);
+
+  if (geo) {
+    if (!req.headers["user-agent"].match(/uptime/) && geo.country == 'US') {
+      var browser = req.headers["user-agent"];
+      var city = (geo ? geo.city : "Unknown");
+      var mobile = (md.mobile() ? 1 : 0);
+  
+      fs.appendFile('./public/visits.txt', '\nBrowser: ' + browser + ' City: ' + city + ' Mobile: ' + mobile, function (err) {
+        if (err) throw err;
+      });
+    }
+  }
+
   res.render('index', {title: "Mittereder Studios"});
 });
 
